fbb338801a1d9461163096f4d9270de8041c82cb
Long Tran <trlong357@gmail.com>
2021-05-19 14:41:52 +0700

    16. Classes, Properties and Methods

--- index.js
+++ index.js
@@ -34,3 +34,20 @@ class Person extends Human {
 const person = new Person();
 person.printName();
 person.printGender();
+
+//ES 7
+class Human2 {
+  gender = "male";
+
+  printGender = () => console.log(this.gender);
+}
+
+class Person2 extends Human2 {
+  name = "tony";
+
+  printName = () => console.log(this.name);
+}
+
+const person2 = new Person2();
+person2.printGender();
+person2.printName();
