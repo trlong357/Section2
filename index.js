3fb0f688c5828342a093fa4bcac30444da1c65bb
Long Tran <trlong357@gmail.com>
2021-05-19 14:27:10 +0700

    13. Arrow Functions

---
+++ index.js
@@ -0,0 +1,12 @@
+// Arrow function
+const sqrt = (number) => Math.sqrt(number);
+
+console.log(sqrt(4));
+
+// normal function
+
+function sqrtNum(number) {
+  console.log(Math.sqrt(number));
+}
+
+sqrtNum(4);
